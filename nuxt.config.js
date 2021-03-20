bb19024c8c4edfbb1ae03de03e6ddbe2749cee09
1327541466@qq.com <zhaoLIANG1211>
2021-03-20 10:21:49 +0800

    测试

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ module.exports = {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
+
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
